8bc4a955ce45917488ec0049a165de9f7541705a
bartek-s13 <43931596+bartek-s13@users.noreply.github.com>
2019-01-09 19:02:10 +0100

    Update cl.c

--- cl.c
+++ cl.c
@@ -2,12 +2,13 @@
 #define MAX_BUFFER 1024
 #define MAX_WORDS 16
 
+
 int main(int argc, char const *argv[])
 {
     key_t key;
-    int id, fifo, i;
+    int id, fifo;
     char buf[MAX_BUFFER];
-    char *words[MAX_WORDS + 1];
+    char *words[MAX_WORDS + 1], *words2[MAX_WORDS + 1];
     // Tworzenie kolejki komunikatów dla klienta
     if ((key = ftok(argv[0], 'T')) == -1) {
         perror("cannot fetch key for client");
@@ -28,43 +29,60 @@ int main(int argc, char const *argv[])
     write(fifo, &id, sizeof(int));
     close(fifo);
 
-    struct Message msg;
+    struct Message sent, received;
     
 
     // Czekanie na odpowiedź serwera
-    if (msgrcv(id, &msg, MSG_SIZE, ANSWER, 0) == -1) {
+    if (msgrcv(id, &received, MSG_SIZE, ANSWER, 0) == -1) {
         perror("cannot receive answer message");
         exit(1);
     }
 
-    if (msg.id_nadawcy == SERWER)
-        printf("%s\n", msg.tresc);
-  
+    if (received.id_nadawcy == SERWER){
+      printf("%s\n", received.tresc);
+      sent.id_nadawcy = received.id_odbiorcy;
+    }
+        
+ if (fork() == 0){
+     if (msgrcv(id, &received, MSG_SIZE, ANSWER, 0) == -1) {
+        perror("cannot receive answer message");
+        exit(1);
+    }     
+   split2(received.tresc, words2);
+   
+   printf("%d: %s", received.id_nadawcy, words2[3]);
+   
+ } 
   
   while(1){
       printf("> ");
-      fgets(buf, MAX, stdin);
-      msg.id_nadawcy = id;
-      n = split(buf, words);
+      fgets(buf, MAX_BUFFER, stdin);
+      split(buf, words);
       if(strcmp(buf,"\n") == 0)
         {
             continue;
         }
         else
-        {
-            if ((strncmp(buf, "createRoom", 10) == 0) || (strncmp(m_usr.tresc, "finger", 6) == 0)){
-              msg.mtype = REQUEST;                
-              msg.id_odbiorcy = 0;
-            }
-          else if(strncmp(buf, "write -u", 8) == 0)){
-            msg.mtype = MSG_USR;
-            msg.id_odbiorcy = words[1];            
+        {           
+          if(strncmp(buf, "write -u", 8) == 0){
+            sent.mtype = MSG_USR;
+            sent.id_odbiorcy = atoi(words[2]);            
           }
-          else if(strncmp(buf, "write -r", 8) == 0)){
-            msg.mtype = MSG_ROOM;
-            msg.id_odbiorcy = words[1];            
-          }   
+          else if(strncmp(buf, "write -r", 8) == 0){
+            sent.mtype = MSG_ROOM;
+            sent.id_odbiorcy = atoi(words[2]);            
+          }  
+          else{
+              sent.mtype = REQUEST;                
+              sent.id_odbiorcy = 0;
+            }
         }
-    
+      strcpy(sent.tresc, buf);
+      
+      if (msgsnd(0, &sent, MSG_SIZE, 0) == -1) {
+                perror("cannot send answer to client message queue");
+                exit(1);
+            }
   }
-  
+    return 0;
+}
